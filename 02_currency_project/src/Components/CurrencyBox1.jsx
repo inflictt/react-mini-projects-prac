2b11e6b3d492d59fe0cb1b96665df826e234ae2b
Saksham Lodha <legitsaksham@gmail.com>
2026-04-05 15:13:50 +0530

    Add 02 currency project

---
+++ 02_currency_project/src/Components/CurrencyBox1.jsx
@@ -0,0 +1,26 @@
+export default function CurrencyBox1() {
+  return (
+    <div className="bg-white rounded-xl p-4 w-full max-w-xl flex justify-between items-start shadow-md">
+      
+      {/* Left Side */}
+      <div className="flex flex-col gap-2">
+        <label className="text-gray-500">From</label>
+        <input
+          type="number"
+          placeholder="0"
+          className="cursor-pointer outline-none text-2xl w-32"
+        />
+      </div>
+
+      {/* Right Side */}
+      <div className="flex flex-col gap-2 items-end">
+        <label className="text-gray-500">Currency Type</label>
+        <select className="cursor-pointer bg-gray-100 rounded-lg px-3 py-2 outline-none">
+          <option>USD</option>
+          <option>CAD</option>
+          <option>MXN</option>
+        </select>
+      </div>
+    </div>
+  );
+}
